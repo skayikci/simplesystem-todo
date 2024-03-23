af26d18f8cf4aae7163caacb0961c02942a6ff73
skayikci <serhat.kayikci@gmail.com>
2024-03-23 13:35:42 +0100

    integrate calls from service to repository

--- src/main/java/com/simplesystem/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/simplesystem/todo/service/impl/TodoServiceImpl.java
@@ -1,13 +1,19 @@
 package com.simplesystem.todo.service.impl;
 
 import com.simplesystem.todo.model.Todo;
+import com.simplesystem.todo.repository.TodoRepository;
 import com.simplesystem.todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService {
+
+    private final TodoRepository todoRepository;
+
     @Override
     public Todo createTodo(Todo todoRequest) {
-        return Todo.builder().id(todoRequest.getId()).build();
+        return todoRepository.save(todoRequest);
     }
 }
--- src/test/java/com/simplesystem/todo/service/impl/TodoServiceImplTest.java
+++ src/test/java/com/simplesystem/todo/service/impl/TodoServiceImplTest.java
@@ -6,17 +6,25 @@ import java.util.UUID;
 
 import com.simplesystem.todo.model.Todo;
 import com.simplesystem.todo.model.TodoStatus;
+import com.simplesystem.todo.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TodoServiceImplTest {
-
     @InjectMocks
     private TodoServiceImpl todoService;
+
+    @Mock
+    private TodoRepository todoRepository;
 
     @Test
     void shouldAddATodoItem() {
@@ -29,10 +37,12 @@ class TodoServiceImplTest {
                 .dueDate(mockCreatedDate.minusDays(10L))
                 .status(TodoStatus.NOT_DONE)
                 .build();
+        when(todoRepository.save(any(Todo.class))).thenReturn(todoItem);
 
         var todoItemVerificationEntity = todoService.createTodo(todoItem);
 
         assertEquals(mockTodoItemId, todoItemVerificationEntity.getId());
+        verify(todoRepository, times(1)).save(todoItem);
     }
 
 }
