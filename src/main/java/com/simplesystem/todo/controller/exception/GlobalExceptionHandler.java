a7a79bc6e269a94e3651381b262e9570f8e8e853
skayikci <serhat.kayikci@gmail.com>
2024-03-24 21:37:53 +0100

    validate non existent entity is handled by handler

--- src/main/java/com/simplesystem/todo/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/simplesystem/todo/controller/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.simplesystem.todo.controller.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +14,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleInvalidEntityException(final InvalidEntityException invalidEntityException) {
         log.error("Handling invalid entity exception: {}", invalidEntityException.getMessage());
         return ResponseEntity.badRequest().body(invalidEntityException.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleInvalidEntityException(final EntityNotFoundException entityNotFoundException) {
+        log.error("Handling entity not found exception: {}", entityNotFoundException.getMessage());
+        return ResponseEntity.badRequest().body(entityNotFoundException.getMessage());
     }
 }
--- src/test/java/com/simplesystem/todo/controller/TodoControllerTest.java
+++ src/test/java/com/simplesystem/todo/controller/TodoControllerTest.java
@@ -10,6 +10,8 @@ import com.simplesystem.todo.controller.adapter.LocalDateTimeTypeAdapter;
 import com.simplesystem.todo.model.Todo;
 import com.simplesystem.todo.model.TodoStatus;
 import com.simplesystem.todo.service.TodoService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -41,103 +43,127 @@ class TodoControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    void shouldCreateATodoItemViaRestPOST() throws Exception {
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
-                .create();
-        Todo todoRequest = getTodoRequest("i will do this via rest post").build();
-        String requestUrl = "/api/v1/todo";
-        MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
-                .post(requestUrl)
-                .content(gson.toJson(todoRequest))
-                .contentType(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(mockMvcRequestBuilders)
-                .andExpect(status().isCreated());
-
-        verify(todoService, times(1)).createTodo(any(Todo.class));
-    }
-
-    @Test
-    void shouldGetInvalidEntityExceptionWhenDueDateIsLessThanCreatedDate() throws Exception {
-        var todoId = UUID.randomUUID();
-        String description = "i will do this with old due date";
-        LocalDateTime createdDate = LocalDateTime.now();
-        LocalDateTime dueDate = createdDate.minusDays(10L);
-        Todo todoRequest = getTodoRequestWithDate(todoId, description, createdDate, dueDate).build();
-        String requestUrl = "/api/v1/todo";
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
-                .create();
-        MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
-                .post(requestUrl)
-                .content(gson.toJson(todoRequest))
-                .contentType(MediaType.APPLICATION_JSON);
-
-
-        mockMvc.perform(mockMvcRequestBuilders)
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$").value("Invalid entity, please check due date"));
-
-        verifyNoInteractions(todoService);
-    }
-
-    @Test
-    void shouldGetInvalidEntityExceptionWhenStatusIsProvidedAsPAST_DUEOnCreation() throws Exception {
-        var todoId = UUID.randomUUID();
-        String description = "i will do this with past_due creation status";
-        Todo todoRequest = getTodoRequest(description).build();
-        todoRequest.setStatus(TodoStatus.PAST_DUE);
-        String requestUrl = "/api/v1/todo";
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
-                .create();
-        MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
-                .post(requestUrl)
-                .content(gson.toJson(todoRequest))
-                .contentType(MediaType.APPLICATION_JSON);
-
-
-        mockMvc.perform(mockMvcRequestBuilders)
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$").value("Invalid entity, please check input status"));
-
-        verifyNoInteractions(todoService);
-    }
-
-    @Test
-    void shouldGetTodoItemById() throws Exception {
-        var todoId = UUID.randomUUID();
-        String description = "i will get this todo by id";
-        LocalDateTime createdDate = LocalDateTime.now();
-        LocalDateTime dueDate = createdDate.minusDays(-10L);
-        Todo todoRequest = getTodoRequestWithDate(todoId, description, createdDate, dueDate).build();
-        String requestUrl = "/api/v1/todo/".concat(todoId.toString());
-        MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
-                .get(requestUrl)
-                .contentType(MediaType.APPLICATION_JSON);
-        when(todoService.getTodoById(todoId)).thenReturn(todoRequest);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy HH:mm:ss");
-
-
-        mockMvc.perform(mockMvcRequestBuilders)
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(equalTo(todoId.toString())))
-                .andExpect(jsonPath("$.description").value(equalTo(description)))
-                .andExpect(jsonPath("$.createdDate").value(equalTo(formatter.format(createdDate))))
-                .andExpect(jsonPath("$.dueDate").value(equalTo(formatter.format(dueDate))))
-                .andExpect(jsonPath("$.status").value(equalTo(TodoStatus.NOT_DONE.name())));
-
-
-        verify(todoService, times(1)).getTodoById(todoId);
-    }
-
     private Todo.TodoBuilder getTodoRequestWithDate(UUID todoId, String description, LocalDateTime createdDate, LocalDateTime dueDate) {
         return getTodoRequest(description).id(todoId).createdDate(createdDate).dueDate(dueDate);
     }
 
     private Todo.TodoBuilder getTodoRequest(String description) {
         return Todo.builder().description(description);
+    }
+
+    @Nested
+    class GetByIdTests {
+        @Test
+        void shouldGetTodoItemById() throws Exception {
+            var todoId = UUID.randomUUID();
+            String description = "i will get this todo by id";
+            LocalDateTime createdDate = LocalDateTime.now();
+            LocalDateTime dueDate = createdDate.minusDays(-10L);
+            Todo todoRequest = getTodoRequestWithDate(todoId, description, createdDate, dueDate).build();
+            String requestUrl = "/api/v1/todo/".concat(todoId.toString());
+            MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
+                    .get(requestUrl)
+                    .contentType(MediaType.APPLICATION_JSON);
+            when(todoService.getTodoById(todoId)).thenReturn(todoRequest);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy HH:mm:ss");
+
+
+            mockMvc.perform(mockMvcRequestBuilders)
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id").value(equalTo(todoId.toString())))
+                    .andExpect(jsonPath("$.description").value(equalTo(description)))
+                    .andExpect(jsonPath("$.createdDate").value(equalTo(formatter.format(createdDate))))
+                    .andExpect(jsonPath("$.dueDate").value(equalTo(formatter.format(dueDate))))
+                    .andExpect(jsonPath("$.status").value(equalTo(TodoStatus.NOT_DONE.name())));
+
+
+            verify(todoService, times(1)).getTodoById(todoId);
+        }
+
+        @Test
+        void shouldGetEntityNotFoundExceptionWhenIdNotFound() throws Exception {
+            var todoId = UUID.randomUUID();
+            String description = "i will get this todo by id";
+            Todo todoRequest = getTodoRequest(description).build();
+            String requestUrl = "/api/v1/todo/".concat(todoId.toString());
+            MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
+                    .get(requestUrl)
+                    .contentType(MediaType.APPLICATION_JSON);
+            when(todoService.getTodoById(todoId)).thenThrow(new EntityNotFoundException("Entity not found, please check input id"));
+
+
+            mockMvc.perform(mockMvcRequestBuilders)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$").value("Entity not found, please check input id"));
+
+            verify(todoService, times(1)).getTodoById(todoId);
+        }
+    }
+
+    @Nested
+    class CreateTodoItemTests {
+        @Test
+        void shouldCreateATodoItemViaRestPOST() throws Exception {
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+                    .create();
+            Todo todoRequest = getTodoRequest("i will do this via rest post").build();
+            String requestUrl = "/api/v1/todo";
+            MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
+                    .post(requestUrl)
+                    .content(gson.toJson(todoRequest))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            mockMvc.perform(mockMvcRequestBuilders)
+                    .andExpect(status().isCreated());
+
+            verify(todoService, times(1)).createTodo(any(Todo.class));
+        }
+
+        @Test
+        void shouldGetInvalidEntityExceptionWhenDueDateIsLessThanCreatedDate() throws Exception {
+            var todoId = UUID.randomUUID();
+            String description = "i will do this with old due date";
+            LocalDateTime createdDate = LocalDateTime.now();
+            LocalDateTime dueDate = createdDate.minusDays(10L);
+            Todo todoRequest = getTodoRequestWithDate(todoId, description, createdDate, dueDate).build();
+            String requestUrl = "/api/v1/todo";
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+                    .create();
+            MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
+                    .post(requestUrl)
+                    .content(gson.toJson(todoRequest))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+
+            mockMvc.perform(mockMvcRequestBuilders)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$").value("Invalid entity, please check due date"));
+
+            verifyNoInteractions(todoService);
+        }
+
+        @Test
+        void shouldGetInvalidEntityExceptionWhenStatusIsProvidedAsPAST_DUEOnCreation() throws Exception {
+            String description = "i will do this with past_due creation status";
+            Todo todoRequest = getTodoRequest(description).build();
+            todoRequest.setStatus(TodoStatus.PAST_DUE);
+            String requestUrl = "/api/v1/todo";
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+                    .create();
+            MockHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders
+                    .post(requestUrl)
+                    .content(gson.toJson(todoRequest))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+
+            mockMvc.perform(mockMvcRequestBuilders)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$").value("Invalid entity, please check input status"));
+
+            verifyNoInteractions(todoService);
+        }
     }
 }
